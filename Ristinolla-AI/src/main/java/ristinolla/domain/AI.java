4af668658e72e634d6048f22499acafea75e29d7
Teemu Närhi <t.a.narhi@gmail.com>
2020-09-18 18:14:44 +0300

    kommentoitiin uusi koodi
    status

--- Ristinolla-AI/src/main/java/ristinolla/domain/AI.java
+++ Ristinolla-AI/src/main/java/ristinolla/domain/AI.java
@@ -4,19 +4,25 @@
  * and open the template in the editor.
  */
 package ristinolla.domain;
-import java.util.Random;
 /**
- *
+ * Luokka sisältää käyttäjää vastaan pelaavan tekoälyn. Metodeilla lasketaan paras mahdollinen siirto, jonka koordinaatit säilötään olion muuttujiin.
  * @author nate
  */
 public class AI {
     int SiirronX;
     int SiirronY;
-    Random satunnainen;
+
+    /**
+     * Luo AI-olion ja asettaa seuraavan siirron arvoksi taulun vasemman alanurkan (tätä arvoa ei kuitenkaan koskaan käytetä sellaisenaan ja on olemassa vain alustustarkoituksiin)
+     */
     public AI() {
         this.SiirronX = 0;
         this.SiirronY = 0;
     }
+    /**
+     * Metodi käy läpi kaikki tällä hetkellä mahdolliset siirrot minimax-metodin avulla ja asettaa oliomuuttujien arvoiksi parhaan siirron x- ja y-koordinaatin.
+     * @param lauta Pelilauta-olio jonka talukkoesityksessä on talessa tämän hetkinen pelitilanne.
+     */
     public void seuraavaSiirto(Pelilauta lauta) {
         
         int parhaanX = -1;
@@ -41,6 +47,16 @@ public class AI {
         this.SiirronX = parhaanX;
         this.SiirronY = parhaanY;
     }
+    /**
+     * Rekursiivinen metodi joka käy läpi kaikki mahdolliset siirrot jokaisella pelipuun tasolla kunnes päätyy tilanteeseen jossa lauta on täynnä, tai jompikumpi pelaaja on voittanut.
+     * Metodi palauttaa arvon joka kertoo mihin tilanteeseen peli päättyy kyseisellä siirolla (olettaen että kaikki sitä mahdollisesti seuraavat siirrot molemmilta pelaajilta ovat "täydellisiä").
+     * @param lauta Pelilauta-olio jonka mukana kulkevassa taulukkoesityksessä on säilöttynä sen hetkinen pelitilanne.
+     * @param tietokoneenVuoro Totuusarvo kertoo onko seuraava siirto tekoälyn, vai käyttäjän tekemä.
+     * @param syvyys Kertoo millä tasolla pelipuuta sillä hetkellä edetään (kuinka monta siirtoa ollaan edetty alkuperäisestä kutsusta). Tämän avulla voidaan muokata metodin palauttamia arvoja siten, että mitä nopeammin kyseinen siirto johtaa voittoon/häviöön, sitä suuremman/pienemmän arvon se saa.
+     * @param x Kutsua edeltäneen siirron x-koordinaatti laudalla.
+     * @param y Kutsua edeltäneen siirron y-koordinaatti laudalla.
+     * @return Arvo kertoo kuinka suotuisa kyseinen siirto (Huom! eli alkuperäistä kutsua EDELTÄVÄ siirto) on tekoälyn kannalta (mitä suurempi arvo, sitä nopeammin se johtaa voittoon ja mitä pienempi, sitä nopeammin se johtaa häviöön).
+     */
     private int minimax(Pelilauta lauta, Boolean tietokoneenVuoro, int syvyys, int x, int y) {
         if (!tietokoneenVuoro && lauta.tarkastaVoitto("O", x, y)) {
             return 500 - syvyys;
