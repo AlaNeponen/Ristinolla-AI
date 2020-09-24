f731c9f62b2fcddda6bdcae19e95865bfbcd69ea
Teemu Närhi <t.a.narhi@gmail.com>
2020-09-24 19:53:19 +0300

    Luotiin käyttöliittymä-luokka

--- Ristinolla-AI/src/main/java/ristinolla/main/Main.java
+++ Ristinolla-AI/src/main/java/ristinolla/main/Main.java
@@ -6,9 +6,7 @@
 package ristinolla.main;
 
 import java.util.Scanner;
-import ristinolla.domain.Pelaaja;
-import ristinolla.domain.Pelilauta;
-import ristinolla.domain.AI;
+import ristinolla.ui.RistinollaUI;
 /**
  *
  * @author nate
@@ -16,63 +14,8 @@ import ristinolla.domain.AI;
 public class Main {
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
-        
-        System.out.println("Syötä pelaajan nimi");
-        String nimi = lukija.nextLine();
-        System.out.println("Syötä pelilaudan leveys ja pituus (yksi numero)");
-        int koko = 0;
-        while (true) {
-            koko = Integer.parseInt(lukija.nextLine());
-            if(koko < 3 || koko > 3) {
-                System.out.println("Psst, Syötä luku 3!");
-            }
-            if (koko >= 3 && koko <= 3) {
-                break;
-            }
-        }
-        
-        Pelaaja käyttäjä = new Pelaaja(nimi, "X");
-        Pelaaja tietokone = new Pelaaja("Tietokone", "O");
-        Pelilauta lauta = new Pelilauta(koko);
-        AI Tekoäly = new AI();
-        
-       while (true) {
- 
-           lauta.tulosta();
-           System.out.println("Syötä siirtosi muodossa: x,y");
-           String siirto = lukija.nextLine();
-           String[] osat = siirto.split(",");
-           int x = Integer.parseInt(osat[0])-1;
-           int y = Integer.parseInt(osat[1])-1;
-           
-           lauta.asetaMerkki(käyttäjä.getMerkki(), x, y);
-           if (lauta.tarkastaVoitto(käyttäjä.getMerkki(), x, y)) {
-               lauta.tulosta();
-               System.out.println("Onnitleut " + käyttäjä.getNimi() + ", voitit pelin!");
-               break;
-           }
-           if (lauta.onkoTäynnä()) {
-               lauta.tulosta();
-               System.out.println("Tasapeli!");
-               break;
-           }
-           Tekoäly.seuraavaSiirto(lauta);
-           lauta.asetaMerkki(tietokone.getMerkki(), Tekoäly.getX(), Tekoäly.getY());
-           if (lauta.tarkastaVoitto(tietokone.getMerkki(), Tekoäly.getX(), Tekoäly.getY())) {
-               lauta.tulosta();
-               System.out.println("Harmi, tietokone voitti tällä kertaa.");
-               break;
-           }
-           if (lauta.onkoTäynnä()) {
-               lauta.tulosta();
-               System.out.println("Tasapeli!");
-               break;
-           }
-       }
-            
-        
-        
-        
+        RistinollaUI ui = new RistinollaUI(lukija);    
+        ui.käynnistä();
     }
     
 }
---
+++ Ristinolla-AI/src/main/java/ristinolla/ui/RistinollaUI.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ristinolla.ui;
+
+import java.util.Scanner;
+import ristinolla.domain.AI;
+import ristinolla.domain.Pelaaja;
+import ristinolla.domain.Pelilauta;
+
+/**
+ *
+ * @author nate
+ */
+public class RistinollaUI {
+    private Scanner lukija;
+    private Pelilauta lauta;
+    private AI tekoäly;
+    private Pelaaja käyttäjä;
+    private Pelaaja tietokone;
+    
+    public RistinollaUI(Scanner lukija) {
+        this.lukija = lukija;
+        this.tekoäly = new AI();
+    }
+    public void käynnistä() {
+        System.out.println("Syötä pelaajan nimi");
+        String nimi = lukija.nextLine();
+        System.out.println("Syötä pelilaudan leveys ja pituus (yksi numero)");
+        int koko = 0;
+        while (true) {
+            koko = Integer.parseInt(lukija.nextLine());
+            if(koko < 3 || koko > 3) {
+                System.out.println("Psst, Syötä luku 3!");
+            }
+            if (koko >= 3 && koko <= 3) {
+                break;
+            }
+        }
+        this.käyttäjä = new Pelaaja(nimi, "X");
+        this.tietokone = new Pelaaja("Tietokone", "O");
+        this.lauta = new Pelilauta(koko);
+        
+        while (true) {
+ 
+           this.lauta.tulosta();
+           System.out.println("Syötä siirtosi muodossa: x,y");
+           String siirto = this.lukija.nextLine();
+           String[] osat = siirto.split(",");
+           int x = Integer.parseInt(osat[0])-1;
+           int y = Integer.parseInt(osat[1])-1;
+           
+           this.lauta.asetaMerkki(this.käyttäjä.getMerkki(), x, y);
+           if (this.lauta.tarkastaVoitto(this.käyttäjä.getMerkki(), x, y)) {
+               this.lauta.tulosta();
+               System.out.println("Onnitleut " + this.käyttäjä.getNimi() + ", voitit pelin!");
+               break;
+           }
+           if (this.lauta.onkoTäynnä()) {
+               this.lauta.tulosta();
+               System.out.println("Tasapeli!");
+               break;
+           }
+           this.tekoäly.seuraavaSiirto(lauta);
+           this.lauta.asetaMerkki(tietokone.getMerkki(), this.tekoäly.getX(), this.tekoäly.getY());
+           if (this.lauta.tarkastaVoitto(this.tietokone.getMerkki(), this.tekoäly.getX(), this.tekoäly.getY())) {
+               this.lauta.tulosta();
+               System.out.println("Harmi, tietokone voitti tällä kertaa.");
+               break;
+           }
+           if (this.lauta.onkoTäynnä()) {
+               this.lauta.tulosta();
+               System.out.println("Tasapeli!");
+               break;
+           }
+       }
+        
+    }
+}
